fd7b33488da249bbaac0b62568cce53488f2da51
kane <mmhd.ozan@gmail.com>
2016-11-19 20:25:22 +0100

    template

--- siteTest/wsgi.py
+++ siteTest/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/kane/djangoTest'
+if path is not in sys.path:
+	sys.path.append(path)
+
+
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteTest.settings")
 
